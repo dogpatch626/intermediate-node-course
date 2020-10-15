0920c8f9f348ceae57fa088ee49c22a1eafe09c5
Daniel <djpepin1299@gmail.com>
2020-10-15 17:53:17 -0400

    add user model and connect to db

--- server.js
+++ server.js
@@ -4,7 +4,21 @@ const bodyParser= require('body-parser');
 const port=8000;
 const app= express();
 
+const User = require('./models/User');
+mongoose.connect('mongodb://localhost/userData')
+
 app.use(bodyParser.json());
+
+const UserSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
+
+})
+
+
+module.exports= mongoose.model('User',UserSchema)
+
 
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
@@ -28,3 +42,4 @@ app.route('/users/:id')
 .delete((req,res)=>{
   // User.findByIdAndDelete()
 })
+
